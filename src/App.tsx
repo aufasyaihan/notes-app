462a71c4756001f34ccebd07f377672d817b2f5e
Aufa Syaihan Azzahidi <aufa.azzahidi@gmail.com>
2024-12-27 21:44:39 +0700

    change initial data

--- src/App.tsx
+++ src/App.tsx
@@ -6,33 +6,11 @@ import Archived from "./components/Archived";
 import Button from "./UI/Button";
 import CreateNote from "./components/CreateNote";
 import SearchBar from "./components/Search";
+import { getInitialData } from "./utils";
 
-const initialData: NoteType[] = [
-    {
-        id: 1,
-        title: "Babel",
-        body: "Babel merupakan tools open-source yang digunakan untuk mengubah sintaks ECMAScript 2015+ menjadi sintaks yang didukung oleh JavaScript engine versi lama. Babel sering dipakai ketika kita menggunakan sintaks terbaru termasuk sintaks JSX.",
-        archived: false,
-        createdAt: "2022-04-14T04:27:34.572Z",
-    },
-    {
-        id: 2,
-        title: "Webpack",
-        body: "Webpack merupakan tools open-source yang digunakan untuk mengelola asset dan dependency pada proyek-proyek JavaScript. Webpack akan membaca semua file JavaScript dan asset lainnya, lalu menggabungkannya menjadi satu file atau beberapa file sesuai dengan konfigurasi yang kita buat.",
-        archived: false,
-        createdAt: "2022-04-14T04:27:34.572Z",
-    },
-    {
-        id: 3,
-        title: "ESLint",
-        body: "ESLint merupakan tools open-source yang digunakan untuk menganalisis kode JavaScript agar sesuai dengan standar yang telah ditentukan. ESLint akan memeriksa kode JavaScript yang kita tulis dan memberikan peringatan jika menemukan kode yang tidak sesuai dengan standar yang telah ditentukan.",
-        archived: true,
-        createdAt: "2022-04-14T04:27:34.572Z",
-    },
-];
 
 function App() {
-    const [notes, setNotes] = useState<NoteType[]>(initialData);
+    const [notes, setNotes] = useState<NoteType[]>(getInitialData());
     const [isModalOpen, setModalOpen] = useState(false);
     const [searchTerm, setSearchTerm] = useState<string>("");
 
